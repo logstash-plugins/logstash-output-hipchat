ad7312c7bfe3b5f231cdc5ae1a95db3f59747b98
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-01-20 15:37:10 -0500

    Removing milestone option

--- lib/logstash/outputs/hipchat.rb
+++ lib/logstash/outputs/hipchat.rb
@@ -7,7 +7,6 @@ require "logstash/outputs/http"
 class LogStash::Outputs::HipChat < LogStash::Outputs::Base
 
   config_name "hipchat"
-  milestone 1
 
   # The HipChat authentication token.
   config :token, :validate => :string, :required => true
